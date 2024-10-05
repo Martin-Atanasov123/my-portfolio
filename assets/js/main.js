4cff884268b3dc61fb6da08aee7be54e88f6ea2f
Martin Atanasov <134212613+Martin-Atanasov123@users.noreply.github.com>
2024-10-05 22:20:00 +0300

    opening certification effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('nav  a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         const targetId = this.getAttribute('href').substring(1);
@@ -12,3 +12,37 @@ document.querySelectorAll('nav  a').forEach(anchor => {
     });
 });
 
+
+// Select all the buttons with class "btn"
+const buttons = document.querySelectorAll('.btn');
+
+// Loop through each button
+buttons.forEach((button) => {
+    // Add a click event listener to each button
+    button.addEventListener('click', () => {
+        // Get the corresponding image
+        const image = button.nextElementSibling;
+
+        // Select the full-screen container and image
+        const fullscreenContainer = document.getElementById('fullscreenImageContainer');
+        const fullscreenImage = document.querySelector('.fullscreen-image');
+
+        // Set the full-screen image source to the clicked image's source
+        fullscreenImage.src = image.src;
+
+        // Show the full-screen container
+        fullscreenContainer.style.display = 'flex';
+    });
+});
+
+// Select the close button
+const closeBtn = document.querySelector('.close');
+
+// Add a click event listener to the close button
+closeBtn.addEventListener('click', () => {
+    // Select the full-screen container
+    const fullscreenContainer = document.getElementById('fullscreenImageContainer');
+
+    // Hide the full-screen container
+    fullscreenContainer.style.display = 'none';
+});
